fcd546744010e2da866d78b0962d711db9ee8f72
Karn Gulati <karngulati@gmail.com>
2018-08-30 23:05:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,26 +6,26 @@
  }
  
   function subtract(a, b) {
-   return (a - b)
+   return (a - b);
  }
  
   function multiply(a, b) {
-   return (a * b)
+   return (a * b);
  }
  
   function divide(a, b) {
-   return (a / b)
+   return (a / b);
  }
  
   function inc(a) {
-   return (a += 1)
+   return (a += 1);
  } 
  
    function dec(a) {
-   return (a -= 1)
+   return (a -= 1);
  } 
  
     function makeInt(a) {
-   return parseInt ('a', 10)
+   return parseInt ('a', 10);
  } 
  
